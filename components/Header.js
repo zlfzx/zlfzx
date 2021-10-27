b1775b76a35ad9acbd8c4ce6400a29172a0c8578
Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2021-10-27 16:21:56 +0700

    landing page

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
 
     return (
-        <header className="h-16 sm:h-20 flex items-center z-30 w-full bg-gray-100">
+        <header className="h-16 sm:h-20 flex items-center z-30 w-full">
             <div className="container mx-auto px-6 flex items-center justify-between">
                 <div className="text-gray-700 dark:text-white font-medium text-3xl flex items-center">
                     <svg width="25" height="25" viewBox="0 0 1792 1792" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Lottie from 'react-lottie';
+import programmer from "../public/lotties/programmer.json"
 
 import { FaGithub } from 'react-icons/fa';
 import { FaDribbble } from 'react-icons/fa';
@@ -13,8 +15,15 @@ import { FaCode } from 'react-icons/fa';
 import Header from '../components/Header';
 
 export default function Home() {
+
+  const lottieOption = {
+    loop: true,
+    autoplay: true,
+    animationData: programmer
+  }
+
   return (
-    <div>
+    <div className="bg-gray-100">
 
       <Head>
         <title>Muhammad Zulfi Izzulhaq</title>
@@ -34,8 +43,22 @@ export default function Home() {
       </Head>
 
       <Header/>
-      <div className="flex flex-col justify-center items-center h-screen py-10 bg-gray-100">
-        <h1 className="text-5xl font-bold font-poppins text-gray-900 antialiased animate-pulse">Hello World!</h1>
+      {/* <div className="flex flex-col justify-center items-center h-screen py-10 bg-gray-100">
+        <h1 className="text-5xl font-poppins text-gray-900 antialiased animate-pulse">Hello <span className="font-bold">World!</span></h1>
+      </div> */}
+
+      <div className="container w-full h-screen grid lg:grid-cols-2 sm:grid-cols-1 mx-auto px-6">
+        <div className="flex flex-col justify-center">
+          <h1 className="text-3xl sm:text-4xl lg:text-5xl xl:text-5xl font-poppins text-gray-800">Hello <span className="font-bold">World!</span></h1>
+          <h2 className="text-xs lg:text-lg sm:text-sm font-poppins text-gray-700 font-medium">I&apos;m Muhammad Zulfi Izzulhaq, Fullstack Web Developer from Pekalongan, Indonesia</h2>
+
+          <p className="font-poppins text-gray-700 text-xs lg:text-sm mt-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam placerat neque eu tellus accumsan, quis dignissim ligula feugiat. Donec eget facilisis ligula. Morbi eget cursus magna. Mauris mollis rhoncus dolor, tristique pellentesque quam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nam et risus tellus.
+          </p>
+        </div>
+        <div className="lg:flex flex-col justify-center sm:hidden hidden">
+          <Lottie options={lottieOption} width={400} height={400} />
+        </div>
       </div>
 
     </div>
