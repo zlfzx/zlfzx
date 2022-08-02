258b6e3fbbe1b8e385f6ed2d80690efa876c9842
Muhammad Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2022-08-02 23:57:28 +0700

    update api

--- pages/api/brankas.js
+++ pages/api/brankas.js
@@ -3,20 +3,17 @@ import firebase from '../../firebase'
 export default function handler(req, res) {
     const firestore = firebase.firestore()
 
-    let message = 'Only POST method will be stored in database'
-    
-    if (req.method == 'POST' && req.body) {
-        firestore.collection('brankas').add({
-            type: req.method,
-            body: req.body,
-            created_at: new Date().toString()
-        })
-        message = 'Successfully stored in database'
-    }
+    firestore.collection('brankas').add({
+        type: req.method,
+        body: req.body,
+        params: req.params,
+        query: req.query,
+        created_at: new Date().toString()
+    })
 
     res.status(200).json({
         status: 'success',
-        message: message,
+        message: 'Successfully stored in database',
         data: req.body
     })
 }
