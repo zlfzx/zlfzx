3a649cdc3ec4a3c8aba50ef1cc39959f9a0a37e2
Muhammad Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2023-02-04 14:25:49 +0700

    update react lottie

--- components/DevAnimation.js
+++ components/DevAnimation.js
@@ -1,15 +1,16 @@
-import Lottie from "react-lottie"
 import DevJson from "./../public/lotties/dev.json"
+import { Player } from '@lottiefiles/react-lottie-player';
 
 export default function DevAnimation() {
 
     return (
         <div className="lg:flex flex-col justify-center">
-            <Lottie options={{
-                loop: true,
-                autoplay: true,
-                animationData: DevJson
-            }} width={400} height={400} />
+            <Player
+                autoplay
+                loop
+                src={DevJson}
+                style={{ height: '400px', width: '400px' }}
+            />
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Header from '@/components/Header'
 import DevAnimation from '../components/DevAnimation';
 
