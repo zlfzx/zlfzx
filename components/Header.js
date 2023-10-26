6f251f9170eb07e034faf537451a738ad39d1fbf
Muhammad Zulfi Izzulhaq <zulfi.izzulhaq@gmail.com>
2023-10-26 14:39:28 +0700

    remove firebase 7 unused page

--- components/Header.js
+++ components/Header.js
@@ -53,9 +53,6 @@ export default function Header() {
                         <Link href="https://json.zlfzx.xyz" className="py-2 px-6 flex hover:text-black" target='_blank'>
                             MyJson
                         </Link>
-                        {/* <Link href="/myjson" className={router.pathname == '/myjson' ? linkActive : link}>
-                            MyJson
-                        </Link> */}
                     </nav>
                     <button className="lg:hidden flex flex-col ml-4" aria-label="Menu" onClick={showNavbar}>
                         <span className="w-6 h-1 bg-gray-800">
@@ -89,7 +86,7 @@ export default function Header() {
                     <Link href="https://blog.zlfzx.xyz" className={linkMobile} target="_blank">
                         Blog
                     </Link>
-                    <Link href="/myjson" className={router.pathname == '/myjson' ? linkActive : link}>
+                    <Link href="https://json.zlfzx.xyz" className={linkMobile} target="_blank">
                         MyJson
                     </Link>
                     </div>
